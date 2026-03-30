eae81eadfa757c038a7c02abb10e2ed1f4f1c778
ritesh-bilip <dasritesh537@gmail.com>
2026-03-31 00:48:44 +0530

    remove auto-migrate use procfile

--- user_service/settings.py
+++ user_service/settings.py
@@ -130,6 +130,3 @@ DATABASES = {
     )
 }
 # Auto-run migrations on startup
-if os.environ.get('RUN_MIGRATIONS'):
-    from django.core.management import call_command
-    call_command('migrate', verbosity=0)
