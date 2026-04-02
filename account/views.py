b76426708d6c6eff12b9f7eade931b392ab3594e
ritesh-bilip <dasritesh537@gmail.com>
2026-04-02 10:11:11 +0530

    fix email auth

--- account/views.py
+++ account/views.py
@@ -9,6 +9,7 @@ from django.core.mail import EmailMessage
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import OTP, Profile
 from .serializer import UserSerializer, ProfileSerializer
